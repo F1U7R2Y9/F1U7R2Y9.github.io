42558c1a1bb0bd374159e0863e628a4a1efbb540
Night Fury ^^ <71229752+F1U7R2Y9@users.noreply.github.com>
2020-12-13 18:38:30 +0530

    initial commit
    uwu

---
+++ ccfury.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+   {
+      char  Heart=3;
+      printf("%c",Heart);
+      printf("\n%c %c",Heart,Heart);
+      printf("\n%c %c %c",Heart,Heart,Heart);
+      printf("\n%c %c %c %c",Heart,Heart,Heart,Heart);
+      printf("\n%c %c %c %c %c",Heart,Heart,Heart,Heart,Heart);
+      return 0;
+   }
